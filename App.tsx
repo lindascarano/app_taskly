0f00b8f77fb249aed6eb403a8896e1abaf952f74
Linda Scarano <lindascarano@gmail.com>
2024-10-30 19:21:46 +0100

    Add TouchableOpacity 'CANCELLA' Button in App.tsx and add some styles

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  
+} from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={styles.itemContainer}
-      >
+      <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log("Premuto")}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>Cancella</Text>
+        </TouchableOpacity>
         <StatusBar style="auto" />
       </View>
     </View>
@@ -21,13 +32,28 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
   },
-  itemContainer:{
+  itemContainer: {
     borderBottomColor: theme.colorAzzurroTeal,
-          borderBottomWidth: 1,
-          paddingHorizontal: 8,
-          paddingVertical: 16,
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  itemText:{
-    fontSize: 18, fontWeight: "200"
-  }
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
 });
--- README.md
+++ README.md
@@ -13,3 +13,18 @@
 ********************************
 **Set up theme.ts**
 - theme.ts : using for global styles (color, padding, margin....)
+
+********************************
+
+**What I have done**
+- node -v
+- nmp -v
+- npm create-expo-app taskly --template -> (Blank (TypeScript) template)
+- mpx expo lint -> ESLint statically analyzes your code to quickly find common problems.
+- npx expo install -- --save-dev prettier eslint-config-prettier eslint-plugin-prettier -> Prettier is an opinionated code formatter. So you wouldn't have to keep track of all the spaces and brackets when writing code. (Now it doens't work for my projegt. Try to fix it)
+
+********************************
+
+**Components**
+
+- Button: TouchableOpacity - Pressable
